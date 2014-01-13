80b43944e34f1f1f1d8a91ae63611fe98621cc64
maerkerma <mathias.maerker@tt-s.com>
2014-01-13 19:27:45 +0100

    Added contenthandler for classource

--- org.uml2abap.adt/src/org/uml2abap/adt/contentHandler/ClassSourceContentHandler.java
+++ org.uml2abap.adt/src/org/uml2abap/adt/contentHandler/ClassSourceContentHandler.java
@@ -1,37 +1,37 @@
 package org.uml2abap.adt.contentHandler;
 
+import java.io.ByteArrayInputStream;
 import java.nio.charset.Charset;
 
 import org.uml2abap.adt.data.ClassSource;
 
+import com.sap.adt.communication.content.AdtMediaType;
 import com.sap.adt.communication.content.IContentHandler;
 import com.sap.adt.communication.message.IMessageBody;
+import com.sap.adt.communication.message.InputStreamMessageBody;
 
 public class ClassSourceContentHandler implements IContentHandler<ClassSource> {
 
 	@Override
 	public ClassSource deserialize(IMessageBody arg0,
 			Class<? extends ClassSource> arg1) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getSupportedContentType() {
-		// TODO Auto-generated method stub
-		return null;
+		return AdtMediaType.TEXT_PLAIN;
 	}
 
 	@Override
 	public Class<ClassSource> getSupportedDataType() {
-		// TODO Auto-generated method stub
-		return null;
+		return ClassSource.class;
 	}
 
 	@Override
-	public IMessageBody serialize(ClassSource arg0, Charset arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public IMessageBody serialize(ClassSource dataObject, Charset arg1) {
+		String source = dataObject.getSourceDefinition();
+		return new InputStreamMessageBody(getSupportedContentType(), new ByteArrayInputStream(source.getBytes()));
 	}
 
 }
